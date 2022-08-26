2af139f9d6ed0ef43866c448a89add01a45f0a7e
Rachel Ardana Putra Ginting <rachelardanaputragitning>
2022-08-26 23:01:08 +0700

    Menambahkan file portfolio

---
+++ dist/script.js
@@ -0,0 +1,70 @@
+// Dark Mode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+const icon = document.querySelector('#icon');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+        icon.classList.add('fa-moon')
+        icon.classList.remove('fa-sun')
+    }else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+        icon.classList.add('fa-sun');
+        icon.classList.remove('fa-moon')
+    }
+});
+
+// Check mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true
+} else {
+    darkToggle.checked = false
+}
+
+// Projects & Skilss
+const projects = document.querySelector('#projects');
+const skills = document.querySelector('#skills');
+const project = document.querySelector('#project');
+const skill = document.querySelector('#skill');
+
+
+projects.addEventListener('click', function () {
+        projects.classList.add('bg-white');
+        projects.classList.add('dark:bg-slate-900');
+        skills.classList.remove('dark:bg-slate-900');
+        skills.classList.remove('bg-white');
+        project.classList.add('block');
+        project.classList.remove('hidden');
+        skill.classList.add('hidden');
+        skill.classList.remove('block');
+});
+
+skills.addEventListener('click', function () {
+    projects.classList.remove('dark:bg-slate-900');
+    skills.classList.add('dark:bg-slate-900');
+    projects.classList.remove('bg-white');
+    skills.classList.add('bg-white');
+    skill.classList.add('block');
+    skill.classList.remove('hidden');
+    project.classList.add('hidden');
+    project.classList.remove('block');
+});
+
+
+// Back to top
+window.onscroll = function () {
+    const toTop = document.querySelector('#toTop');
+    const mode = document.querySelector('#mode');
+    const fixed = mode.offsetTop;
+
+    if (window.pageYOffset > fixed) {
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    }else {
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+  content: ['index.html'],
+  theme: {
+    container: {
+        center: true,
+        padding: '16px'
+    },
+    fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['Poppins', 'serif'],
+      },
+    extend: {},
+  },
+  plugins: [],
+}
